977dea54cd3dbe1b5f5469fb181ab21e117cf8fd
root <root@vmi554311.contaboserver.net>
2021-04-24 06:11:05 -0500

    ksss2

--- salvio/settings.py
+++ salvio/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'business',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'USER': 'ventura',
+        'PASSWORD': 'P4sw0rd',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
